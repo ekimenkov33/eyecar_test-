1effad647845863083ee95fed254ea98201cf35a
unknown <alex.ekimenkov@gmail.com>
2022-09-29 18:53:38 +0300

    add ret

---
+++ ret.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame,(640,480))
+    
+    cv2.imshow("Frame", frame)
+    print ("ok")
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows
